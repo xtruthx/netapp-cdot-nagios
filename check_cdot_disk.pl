6f59b22182a6489a2c6fff881fcc137a4fa6ae3d
thereseho <30930469+thereseho@users.noreply.github.com>
2019-01-08 09:40:45 +0100

    Combined script for spare disk check and disk check

--- check_cdot_disk.pl
+++ check_cdot_disk.pl
@@ -14,6 +14,7 @@ use strict;
 use warnings;
 
 use lib "/usr/lib/netapp-manageability-sdk/lib/perl/NetApp";
+
 use NaServer;
 use NaElement;
 use Getopt::Long qw(:config no_ignore_case);
@@ -21,123 +22,157 @@ use Getopt::Long qw(:config no_ignore_case);
 my $critical = 2;
 my $warning = 1;
 GetOptions(
-    'H|hostname=s'  => \my $Hostname,
-    'u|username=s'  => \my $Username,
-    'p|password=s'  => \my $Password,
-    'w|warning=i'   => \$warning,
-    'c|critical=i'  => \$critical,
+    'H|hostname=s' => \my $Hostname,
+    'u|username=s' => \my $Username,
+    'p|password=s' => \my $Password,
+    'w|warning=i' => \$warning,
+    'c|critical=i' => \$critical,
     'd|diskcount=i' => \my $Diskcount,
-    'P|perf'        => \my $perf,
-    'h|help'        => sub { exec perldoc => -F => $0 or die "Cannot execute perldoc: $!\n"; },
+	'exclude=s'	 =>	\my @excludelistarray,
+    'regexp'            => \my $regexp,
+    'P|perf' => \my $perf,
+    'h|help'     => sub { exec perldoc => -F => $0 or die "Cannot execute perldoc: $!\n"; },
 ) or Error("$0: Error in command line arguments\n");
 
+# Filter through full names or regex
+my %Excludelist;
+@Excludelist{@excludelistarray}=();
+my $excludeliststr = join "|", @excludelistarray;
+
 sub Error {
-    print "$0: ".$_[0]."\n";
+    print "$0: " . $_[0] . "\n";
     exit 2;
 }
-Error( 'Option --hostname needed!' ) unless $Hostname;
-Error( 'Option --username needed!' ) unless $Username;
-Error( 'Option --password needed!' ) unless $Password;
+Error('Option --hostname needed!') unless $Hostname;
+Error('Option --username needed!') unless $Username;
+Error('Option --password needed!') unless $Password;
 
 my $s = NaServer->new( $Hostname, 1, 3 );
-$s->set_transport_type( "HTTPS" );
-$s->set_style( "LOGIN" );
+$s->set_transport_type("HTTPS");
+$s->set_style("LOGIN");
 $s->set_admin_user( $Username, $Password );
 
-my $iterator = NaElement->new( "storage-disk-get-iter" );
-my $tag_elem = NaElement->new( "tag" );
-$iterator->child_add( $tag_elem );
+my $iterator = NaElement->new("storage-disk-get-iter");
+my $tag_elem = NaElement->new("tag");
+$iterator->child_add($tag_elem);
 
 my $next = "";
 my $disk_count = 0;
-my @disk_list;
-my %inventory = ( 'Spare', 0, 'Rebuilding', 0, 'Aggregate', 0, 'Failed', 0);
+my @failed_disks;
+my @not_zeroed_disks;
+my @unassigned_disks;
+my %inventory = ( 'Spare', 0, 'Rebuilding', 0, 'Aggregate', 0, 'Failed', 0, 'Not_zeroed', 0, 'Unassigned', 0);
 
-while(defined( $next )){
-    unless ($next eq "") {
-        $tag_elem->set_content( $next );
+while(defined($next)){
+    unless($next eq ""){
+        $tag_elem->set_content($next);    
     }
 
-    $iterator->child_add_string( "max-records", 100 );
-    my $output = $s->invoke_elem( $iterator );
+    $iterator->child_add_string("max-records", 100);
+    my $output = $s->invoke_elem($iterator);
 
-    if ($output->results_errno != 0) {
-        my $r = $output->results_reason();
-        print "UNKNOWN: $r\n";
-        exit 3;
-    }
+	if ($output->results_errno != 0) {
+    	my $r = $output->results_reason();
+    	print "UNKNOWN: $r\n";
+    	exit 3;
+	}
+	
+	my $disks = $output->child_get("attributes-list");
+	my @result = $disks->children_get();
+	
+	foreach my $disk (@result) {
+	    my $raid_type = $disk->child_get("disk-raid-info");
+	    my $container = $raid_type->child_get_string('container-type');
+		my $disk_name = $disk->child_get_string('disk-name');
 
-    unless($output->child_get_int( "num-records" ) eq "0") {
+		next if exists $Excludelist{$disk_name};
+	
+		if ($regexp and $excludeliststr) {
+			next if ($disk_name =~ m/$excludeliststr/);
+		}
+	
+	    $disk_count++;
+	
+	    if ( $container eq 'shared' ) {
+	        # Dig deeper
+			my $shared_info = $raid_type->child_get("disk-shared-info");
+			my $temp = $shared_info->child_get_string('is-reconstructing');
+			if ($temp eq 'true') {
+				$inventory{'Rebuilding'}++;
+			} else {
+				$temp = $shared_info->child_get_string('is-replacing');
+				if ($temp eq 'true') {
+				# Also count as Rebuilding
+				$inventory{'Rebuilding'}++;
+				} else {
+					$inventory{'Aggregate'}++;
+				}
+			}
+	    } elsif ( $container eq 'spare' ) {
+	    	$inventory{'Spare'}++;
+			my $spare_info = $raid_type->child_get("disk-spare-info");
+			my $zeroed = $spare_info->child_get_string('is-zeroed');
 
-        my $disks = $output->child_get( "attributes-list" );
-        my @result = $disks->children_get();
-
-        foreach my $disk (@result) {
-            my $raid_type = $disk->child_get( "disk-raid-info" );
-            my $container = $raid_type->child_get_string( "container-type" );
-
-            $disk_count++;
-
-            if ( $container eq 'shared' ) {
-                # Dig deeper
-                my $shared_info = $raid_type->child_get( "disk-shared-info" );
-                my $temp = $shared_info->child_get_string( "is-reconstructing" );
-                if ($temp eq 'true') {
-                    $inventory{'Rebuilding'}++;
-                } else {
-                    $temp = $shared_info->child_get_string( "is-replacing" );
-                    if ($temp eq 'true') {
-                        # Also count as Rebuilding
-                        $inventory{'Rebuilding'}++;
-                    } else {
-                        $inventory{'Aggregate'}++;
-                    }
-                }
-            } elsif ( $container eq 'spare' ) {
-                $inventory{'Spare'}++;
-            } elsif ( $container eq 'aggregate' ) {
-                # Dig deeper
-                my $aggr_info = $raid_type->child_get( "disk-aggregate-info" );
-                my $temp = $aggr_info->child_get_string( "is-reconstructing" );
-                if ($temp eq 'true') {
-                    $inventory{'Rebuilding'}++;
-                } else {
-                    $temp = $aggr_info->child_get_string( "is-replacing" );
-                    if ($temp eq 'true') {
-                        # Also count as Rebuilding
-                        $inventory{'Rebuilding'}++;
-                    } else {
-                        $inventory{'Aggregate'}++;
-                    }
-                }
-            } else {
-                my $owner = $disk->child_get( "disk-ownership-info" );
-                my $diskstate = $owner->child_get_string( "is-failed" );
-                if (( $diskstate eq 'true' ) && ($container ne 'maintenance')) {
-                    push @disk_list, $disk->child_get_string( "disk-name" );
-                    $inventory{'Failed'}++;
-                } else {
-                    $inventory{'Aggregate'}++;
-                }
-            }
-        }
-    }
-    $next = $output->child_get_string( "next-tag" );
-
+			if($zeroed eq 'false'){
+				push @not_zeroed_disks, $disk_name;
+				$inventory{'Not_zeroed'}++;
+			}
+		} elsif( $container eq 'unassigned' ){
+			push @unassigned_disks, $disk_name;
+			$inventory{'Unassigned'}++;
+	    } elsif ( $container eq 'aggregate' ) {
+	    	# Dig deeper
+			my $aggr_info = $raid_type->child_get('disk-aggregate-info');
+			my $temp = $aggr_info->child_get_string('is-reconstructing');
+			if ($temp eq 'true') {
+				$inventory{'Rebuilding'}++;
+			} else {
+				$temp = $aggr_info->child_get_string('is-replacing');
+				if ($temp eq 'true') {
+				# Also count as Rebuilding
+				$inventory{'Rebuilding'}++;
+				} else {
+					$inventory{'Aggregate'}++;
+				}
+			}
+	    } else {
+			my $owner = $disk->child_get("disk-ownership-info");
+			my $diskstate = $owner->child_get_string('is-failed');
+			if (( $diskstate eq 'true' ) && ($container ne 'maintenance')) {
+				push @failed_disks, $disk_name;
+				$inventory{'Failed'}++;
+			} else {
+				$inventory{'Aggregate'}++;
+			}
+	    }
+	}
+	$next = $output->child_get_string("next-tag");
 }
 
 my $perfdatastr='';
-$perfdatastr = sprintf(" | Aggregate=%dDisks Spare=%dDisks Rebuilding=%dDisks Failed=%dDisks",
+$perfdatastr = sprintf(" | Aggregate=%d Disks; Spare=%d Disks; Rebuilding=%d Disks; Failed=%d Disks",
     $inventory{'Aggregate'}, $inventory{'Spare'}, $inventory{'Rebuilding'}, $inventory{'Failed'}
 ) if ($perf);
 
-if ( scalar @disk_list >= $critical ) {
-    print "CRITICAL: " . @disk_list . ' failed disk(s): ' . join( ', ', @disk_list ) . $perfdatastr ."\n";
+if ( scalar @failed_disks >= $critical) {
+	print "CRITICAL: \n" . @failed_disks . " failed disk(s):\n" . join( "\n", @failed_disks . $perfdatastr ."\n";
     exit 2;
-} elsif ( scalar @disk_list >= $warning ) {
-    print "WARNING: " . @disk_list .' failed disk(s): ' .join( ', ', @disk_list ) . $perfdatastr ."\n";
+}
+if ( scalar @failed_disks >= $warning || scalar @not_zeroed_disks >= $warning || scalar @unassigned_disks >= $warning ) {
+	print "WARNING: ";
+	if ( scalar @failed_disks >= $warning ) {
+		print "\n" . @failed_disks . " failed disk(s):\n" . join( "\n", @failed_disks );
+	}
+	if ( scalar @not_zeroed_disks >= $warning ) {
+		print "\n\n" . @not_zeroed_disks . " zeroed disk(s):\n" . join( "\n", @not_zeroed_disks );
+	}
+	if ( scalar @unassigned_disks >= $warning ) {
+		print "\n\n" . @unassigned_disks . " unassigned disk(s):\n" . join( "\n", @unassigned_disks );
+	}
+
+	print $perfdatastr ."\n";
     exit 1;
-} elsif(($Diskcount) && ($Diskcount ne $disk_count)) {
+} elsif(($Diskcount) && ($Diskcount ne $disk_count)){
     my $diff = $Diskcount-$disk_count;
     print "CRITICAL: $diff disk(s) missing".$perfdatastr."\n";
     exit 2;
@@ -168,7 +203,7 @@ Checks if there are some damaged disks.
 
 =item -H | --hostname FQDN
 
-The Hostname of the NetApp to monitor (Cluster or Node MGMT)
+The Hostname of the NetApp to monitor
 
 =item -u | --username USERNAME
 
